769bbf0a1ada40514782af5e9796eddde8ff6336
Prathamesh Chaughule <166697280+PrathameshChaughule@users.noreply.github.com>
2025-04-14 19:40:59 +0530

    Commit

---
+++ src/university/management/system/About.java
@@ -0,0 +1,39 @@
+
+package university.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class About extends JFrame{
+    
+    About(){
+        setSize(700, 500);
+        setLocation(400, 150);
+        getContentPane().setBackground(Color.WHITE);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/about.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(300, 200, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(350, 10, 300, 200);
+        add(image);
+        
+        JLabel heading = new JLabel("<html>University<br/>Management System</html>");
+        heading.setBounds(70, 20, 300, 130);
+        heading.setFont(new Font("Tahoma", Font.BOLD, 30));
+        add(heading);
+        
+        JLabel name = new JLabel("Developed By: Prathemsh Chaughule");
+        name.setBounds(70, 220, 650, 40);
+        name.setFont(new Font("Tahoma", Font.BOLD, 30));
+        add(name);
+        
+        setLayout(null);
+        
+        setVisible(true);
+    }
+    
+    public static void main(String[] args){
+        new About();
+    }
+}
